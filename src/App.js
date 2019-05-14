a01abfe553c66d455e80393c637dd95fd7babcae
Estultz731 <elizablue731@gmail.com>
2019-05-14 11:36:44 -0400

    added images and navbar and html

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Nav = () => 
+(<nav>
+  <a className="left" href="#date">Acme Dating Site</a>
+  <a className="right" href="#contact">Contact</a>
+  <a className="right" href="#social">Social</a>
+</nav>)
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Nav />
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-sm-4">
+              <div className="card">
+                <div className="top"><h2>Lorem</h2></div>
+                <div className="bottom"><img className="picture" src="./images/dance.jpg" /></div>
+              </div>
+            </div>
+            <div className="col-lg-12 col-sm-4">
+              <div className="card">
+                <div className="top"><h2>Ipsum</h2></div>
+                <div className="bottom"><img className="picture" src="./images/hug.jpg" /></div>
+              </div>
+            </div>
+            <div className="col-lg-12 col-sm-4">
+              <div className="card">
+                <div className="top"><h2>Nunc</h2></div>
+                <div className="bottom"><img className="picture" src="./images/wedding.jpg" /></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
